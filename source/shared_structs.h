7c744ba61ab0dbdb66d8b0e06d81bb8f2a61631d
Angela Cui <ayc62@cornell.edu>
2023-03-27 21:06:27 -0400

    first commit

---
+++ source/shared_structs.h
@@ -0,0 +1,21 @@
+#ifndef __SHARED_STRUCTS_H__
+#define __SHARED_STRUCTS_H__
+
+/** Implement your structs here */
+
+/**
+ * This structure holds the process structure information
+ */
+struct process_state {
+	
+};
+
+/**
+ * This defines the lock structure
+ */
+typedef struct lock_state {
+	
+} lock_t;
+
+
+#endif
